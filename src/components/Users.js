62f7972d8d1309e54bcccccc708497e7b10e0a64
Matt <mwbertolucci@gmail.com>
2023-04-05 17:53:28 -0400

    Add extrareducers

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,25 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "../redux/users/usersSlice"
+import { useEffect } from "react";
 
 const Users = () => {
-  const { users, isLoading, error } = useSelector((store) => store.users);
+  const { users, isLoading, error } = useSelector((store) => store);
+  const dispatch = useDispatch();
   
+  useEffect(() =>{
+    dispatch(fetchUsers());
+  }, [])
+
   return (
     <>
     <h1>Hello world</h1>
-    {isLoading && <div>Loading...</div>}
+    {isLoading && <h2>Loading...</h2>}
+    {error && <h2>Something went wrong!</h2>}
+    <ul>
+      {users.results.map((user) => (
+        <li key={user.login.uuid}>{user.name.first} {user.name.last}</li>
+      ))}
+    </ul>
     </>
     );
 }
--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -1,15 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const URL = 'https://randomuser.me/api/?results=5';
 
 const initialState = {
-  users: [],
+  users: [
+    {
+      id: 1,
+      name: 'john',
+      lastName: 'Doe',
+    }
+  ],
   isLoading: true,
-  error: undefined,
+  error: false,
 };
+
+const fetchUsers = createAsyncThunk(
+  'users/fetchUsers',
+  async () => {
+    try {
+      const res = await fetch(URL);
+      const data = await res.json();
+      return data;
+    } catch(error) {
+      return error;
+    }
+  }
+);
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
-  extraReducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(fetchUsers.fulfilled, (state, action) => {
+      state.users = action.payload;
+      state.isLoading = false;
+    })
+    .addCase(fetchUsers.rejected, (state) => {
+      state.error = true;
+      state.isLoading = false;
+    })
+  }
 });
 
+export { fetchUsers };
 export const usersReducer = usersSlice.reducer;
